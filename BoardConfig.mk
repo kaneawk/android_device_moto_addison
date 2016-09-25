03d048ca956dbb4de1d7f727b5fb5fd913c082e4
kaneawk <kaneawk@gmail.com>
2016-09-25 13:31:52 +0800

    Add qseecomd blobs & fixes minor issues

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,18 +42,30 @@ TARGET_CPU_SMP := true
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Keymaster - Wait for qseecom to load
+# TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Prebuilts
+TARGET_PREBUILT_KERNEL := device/moto/addison/zImage
+BOARD_CUSTOM_BOOTIMG_MK := device/moto/addison/mkbootimg.mk
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --kernel_offset 0x00008000 --second_offset 0x00f00000 --dt device/moto/addison/dt.img
-BOARD_CUSTOM_BOOTIMG_MK := device/moto/addison/mkbootimg.mk
-TARGET_PREBUILT_KERNEL := device/moto/addison/kernel
+BOARD_MKBOOTIMG_ARGS :=  --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --dt device/moto/addison/dt.img
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x02000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x02000000
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x001000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x001019000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x100000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0xD5B07B000
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x010000000
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Disable secure discard because it's SLOW
 BOARD_SUPPRESS_SECURE_ERASE := true
@@ -61,14 +73,8 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 # charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-TARGET_CRYPTFS_HW_PATH := device/moto/addison/cryptfs_hw
-# Keymaster - Wait for qseecom to load
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := device/moto/addison/twrp.fstab
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
@@ -81,10 +87,12 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_NEW_ION_HEAP := true
+TW_DEFAULT_BRIGHTNESS := 80
 TW_INCLUDE_NTFS_3G := true
 TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_CRYPTO := true
+TW_NO_USB_STORAGE := true
+#TW_USE_TOOLBOX    := true
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
--- omni_addison.mk
+++ omni_addison.mk
@@ -25,14 +25,19 @@ PRODUCT_RELEASE_NAME := addison
 # Charger
 PRODUCT_PACKAGES += charger charger_res_images
 
-PRODUCT_COPY_FILES += \
-    device/moto/addison/kernel:kernel \
-    device/moto/addison/dt.img:dt.img \
-	device/moto/addison/twrp.fstab:root/etc/twrp.fstab
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_addison
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := addison
-PRODUCT_MODEL := Moto Z Play
+PRODUCT_MODEL := XT1635
 PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+TARGET_VENDOR_PRODUCT_NAME := addison
+TARGET_VENDOR_DEVICE_NAME := addison
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=addison PRODUCT_NAME=addison
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=motorola/addison/addison:6.0.1/MPN24.104-25/25:user/release-keys \
+    PRIVATE_BUILD_DESC="addison-user 6.0.1 MPN24.104-25 25 release-keys"
